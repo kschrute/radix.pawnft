11bc17d92957dc78ed3758854205d7fdd7ccd56b
K S <konstantin.shkut@strongblock.io>
2024-09-27 10:36:44 +0200

    debug

--- packages/web/src/app/dev/page.tsx
+++ packages/web/src/app/dev/page.tsx
@@ -1,11 +1,16 @@
 import RadixDevTools from '@/components/RadixDevTools'
 import { Box, Heading } from '@chakra-ui/react'
+import { Debug } from '@/components/Debug'
+import config from '@/config'
 
 export default function Page() {
   return (
     <Box>
       <Heading mb={10}>Dev</Heading>
       <RadixDevTools />
+      <Debug data={{
+        config
+      }} />
     </Box>
   )
 }
