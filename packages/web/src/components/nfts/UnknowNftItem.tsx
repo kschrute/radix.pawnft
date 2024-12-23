312560247f0c65b277dcb826b4b5623d1bfd3ce0
K S <konstantin.shkut@strongblock.io>
2024-12-23 12:08:23 +0000

    ui improvements

--- packages/web/src/components/nfts/UnknowNftItem.tsx
+++ packages/web/src/components/nfts/UnknowNftItem.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import type { UnknownNFT } from '@/types'
-import { Badge, Card, CardHeader, Heading } from '@chakra-ui/react'
+import { Badge, Card, CardHeader, Heading, Image } from '@chakra-ui/react'
 import React from 'react'
 
 type Props = {
@@ -13,6 +13,7 @@ type Props = {
 export default function UnknowNftItem({ nft, isActive, onClickSelect }: Props) {
   const { id, type, data } = nft
   const name = (data?.name as string) ?? id ?? 'Unknown'
+  const image = data.key_image_url as string ?? 'https://i.imgur.com/DehtNMl.png'
 
   const handleSelect = async () => {
     onClickSelect?.(nft)
@@ -39,6 +40,7 @@ export default function UnknowNftItem({ nft, isActive, onClickSelect }: Props) {
         <Heading size="md">
           <Badge>{type}</Badge>
           &nbsp;{name}
+          <Image mt={2} src={image} alt="NFT" />
         </Heading>
       </CardHeader>
     </Card>
